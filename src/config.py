0e556d64888a52d00f15351db9e816972f0831a0
Lionel H <me@nullbyte.be>
2025-12-06 10:00:49 +0100

    ci: add mulitarch docker image build
    Allow to run in different env
    
    I also update the readme to make mcp use the .env directly to avoid
    exposing the secrets in .mcp.json file.
    
    Signed-off-by: Lionel H <me@nullbyte.be>

--- src/config.py
+++ src/config.py
@@ -11,7 +11,7 @@ class Config:
     MEMBER_ID = os.getenv("MEMBER_ID")
     ACCESS_KEY = os.getenv("ACCESS_KEY")
     SECRET_KEY = os.getenv("SECRET_KEY")
-    TESTNET = False
+    TESTNET = os.getenv("TESTNET", False).lower() == "true"
 
     @classmethod
     def log_config(cls):
